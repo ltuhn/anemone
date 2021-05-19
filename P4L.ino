0e03f67d4f71019ba969021010575df844f57455
ltuhn <80274740+ltuhn@users.noreply.github.com>
2021-05-19 10:26:31 +0200

    Update P4L.ino

--- P4L.ino
+++ P4L.ino
@@ -84,10 +84,39 @@ void happy() {
 }
 
 void sad() {
-
+  
+  //armer
+  for (pos = 0; pos <= 180; pos += 1) {
+      myservo.write(pos);
+  }
+  delay(500);
+  for (int i = 0; i < 2; i++) {
+      for (pos = 180; pos >= 0; pos -= 1) {
+          myservo.write(pos);
+      }
+      delay(150);
+      for (pos = 0; pos <= 180; pos += 1) {
+          myservo.write(pos);
+      }
+      delay(150);
+      for (pos = 180; pos >= 0; pos -= 1) {
+          myservo.write(pos);
+      }   
+  }
 }
 
 void angry() {
-
+  
+  //armer
+  for (int i = 0; i < 2; i++) {
+      for (pos = 0; pos <= 180; pos += 1) {
+          myservo.write(pos);
+      }
+      delay(300);
+      for (pos = 180; pos >= 0; pos -= 1) {
+          myservo.write(pos);
+      }
+      delay(300);             
+  } 
 }
    
