796cc045fc4105ac978aee053e035a049aba9185
henryette <62426124+henryette@users.noreply.github.com>
2021-05-20 14:41:50 +0200

    Update P4L.ino

--- P4L.ino
+++ P4L.ino
@@ -1,4 +1,11 @@
 #include <Servo.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
 
 Servo arm;
 int pos = 0;
